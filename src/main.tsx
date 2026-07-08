ad34b0a8a32ec4f4e2b695a10c02629b5bbb9484
LunaKim <34995867+parkchewon486@users.noreply.github.com>
2026-07-08 18:14:31 +0900

    Load audience value section

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './index.css';
 
 import App from './App.tsx';
 import './mobile-fix.css';
+import './audience-info.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
