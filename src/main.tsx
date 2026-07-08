080b3156f83fe4b65b35aab48401da795647c5b6
LunaKim <34995867+parkchewon486@users.noreply.github.com>
2026-07-08 20:25:16 +0900

    Load Fujifilm runtime extension

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './index.css';
 import App from './App.tsx';
 import './mobile-fix.css';
 import './audience-info.css';
+import './fuji-runtime.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
